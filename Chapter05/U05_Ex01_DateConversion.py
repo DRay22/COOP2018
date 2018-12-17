c12a0e8fa2a9030cae9d945c979904c3cc4ce86a
Donovan Ray <donovan.r22@parishepiscopal.org>
2018-12-17 10:20:01 -0600

    Fixed errors with code and output

--- Chapter05/U05_Ex01_DateConversion.py
+++ Chapter05/U05_Ex01_DateConversion.py
@@ -12,47 +12,29 @@
 #    Chapter: #05
 #
 # Program Description
-#   This program will convert a numerical date into a date displaying month in an abbreviated form
+#   This program will convert a numerical date into a date displaying month as a string
 #
-#
-#
-# Algorithm (pseudocode)
-#   Introduce program
-#   Make list for month
-#   Make list for days
-#   Get input as int in form of dd/mm/yy (seperate for input for each (day, month, year)
-#   Make dateConv1, printing all the numbers in a list as strings
-#   Find month from list using input of month as a position, make variable name monthConv(string)
-#   make variable called yearConv, where it takes the last two numbers of the year input
-#   make variable called dateConv2, where it lists all the Conv variables
-#   Print dayConv1 and dayConv2
-
+# Algorithm
+#   get the day, month, and year as numbers
+#   Make first date using numbers
+#   Make list of months, select moth with month number - 1
+#   Make date2 using the month string
+#   Print using string formatting
 
 def main():
-    #   Introduce program
-    print("This program will convert a numerical date into a date displaying month in a word format and will display "
-          "both")
+    # get the day month and year as numbers
+    day = int(input("Enter the day number: "))
+    month = int(input("Enter the month number: "))
+    year = int(input("Enter the year: "))
 
-    #   Make list for month
-    monthlist = ["January", "February", "March", "April", "May", "June", "July", "August",
-                 "September", "October", "November", "December"]
+    date1 = str(month)+"/"+str(day)+"/"+str(year)
 
-    #   Make list for days
-    daylist = ["First", "Second", "Third", "Fourth", "Fifth", "Sixth", "Seventh", "Eighth", "Ninth", "Tenth",
-               "Eleventh", "Twelfth", "Thirteenth", "Fourteenth", "Fifteenth", "Sixteenth", "Seventeenth",
-               "Eighteenth", "Nineteenth", "Twentieth", "Twenty First", "Twenty Second", "Twenty Third",
-               "Twenty Fourth", "Twenty Fifth", "Twenty Sixth", "Twenty Seventh", "Twenty Eighth",
-               "Twenty Ninth", "Thirtieth", "Thirty First"]
-    month = int(input("What is the number of the month?   "))
-    day = int(input("What is the number of the day?    "))
-    year = input("What year is it?    ")
-    DateConv1 = [str(month), str(day), str(year)]
-    yearConv = year[2:4]
-    monthConv = monthlist[month - 1]
-    dayConv = daylist[day - 1]
-    DateConv2 = [monthConv + ',' + dayConv+',' + yearConv]
-    print("The date is:", DateConv1, "or", DateConv2)
+    months = ["January", "February", "March", "April",
+              "May", "June", "July", "August",
+              "September", "October", "November", "December"]
+    monthStr = months[month-1]
+    date2 = monthStr+" " + str(day) + ", " + str(year)
 
+    print("The date is {0} or {1}.".format(date1, date2))
 
 main()
-
